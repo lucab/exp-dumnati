5ced7acddba981255fa5f8f2744ff0adc498bcd5
Luca Bruno <luca.bruno@coreos.com>
2019-07-20 08:42:15 +0000

    dumnati: add basic requests counting metrics

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,11 @@ extern crate serde;
 extern crate serde_derive;
 extern crate serde_json;
 extern crate structopt;
+#[macro_use]
+extern crate prometheus;
 
 mod graph;
+mod metrics;
 mod scraper;
 
 use actix::prelude::*;
@@ -20,8 +23,24 @@ use actix_web::{http::Method, middleware::Logger, server, App};
 use actix_web::{HttpRequest, HttpResponse};
 use failure::{Error, Fallible};
 use futures::prelude::*;
+use lazy_static::lazy_static;
+use prometheus::IntCounter;
 use std::net::{IpAddr, Ipv4Addr};
+use std::sync::Arc;
 use structopt::StructOpt;
+
+lazy_static! {
+    static ref V1_GRAPH_INCOMING_REQS: IntCounter = register_int_counter!(opts!(
+        "dumnati_v1_graph_incoming_requests_total",
+        "Total number of incoming HTTP client request to /v1/graph"
+    ))
+    .unwrap();
+    static ref UNIQUE_IDS: IntCounter = register_int_counter!(opts!(
+        "dumnati_v1_graph_unique_uuids_total",
+        "Total number of unique node UUIDs (per-instance Bloom filter)."
+    ))
+    .unwrap();
+}
 
 fn main() -> Fallible<()> {
     env_logger::Builder::from_default_env().try_init()?;
@@ -34,12 +53,21 @@ fn main() -> Fallible<()> {
 
     let scraper_addr = scraper::Scraper::new("testing")?.start();
 
-    let app_state = AppState { scraper_addr };
+    let node_population = Arc::new(cbloom::Filter::new(10 * 1024 * 1024, 1_000_000));
+    let app_state = AppState {
+        scraper_addr,
+        population: Arc::clone(&node_population),
+    };
 
     server::new(move || {
         App::with_state(app_state.clone())
             .middleware(Logger::default())
             .route("/v1/graph", Method::GET, serve_graph)
+            .route(
+                "/private-will-move/metrics",
+                Method::GET,
+                metrics::serve_metrics,
+            )
     })
     .bind((IpAddr::from(Ipv4Addr::UNSPECIFIED), port))?
     .start();
@@ -51,16 +79,21 @@ fn main() -> Fallible<()> {
 #[derive(Clone, Debug)]
 pub(crate) struct AppState {
     scraper_addr: Addr<scraper::Scraper>,
+    population: Arc<cbloom::Filter>,
 }
 
 pub(crate) fn serve_graph(
     req: HttpRequest<AppState>,
 ) -> Box<Future<Item = HttpResponse, Error = Error>> {
-    let resp = req
+    record_metrics(&req);
+
+    let cached_graph = req
         .state()
         .scraper_addr
         .send(scraper::GetCachedGraph::default())
-        .flatten()
+        .flatten();
+
+    let resp = cached_graph
         .and_then(|graph| {
             serde_json::to_string_pretty(&graph).map_err(|e| failure::format_err!("{}", e))
         })
@@ -71,6 +104,24 @@ pub(crate) fn serve_graph(
         });
 
     Box::new(resp)
+}
+
+pub(crate) fn record_metrics(req: &HttpRequest<AppState>) {
+    use std::collections::hash_map::DefaultHasher;
+    use std::hash::{Hash, Hasher};
+
+    V1_GRAPH_INCOMING_REQS.inc();
+
+    let population = &req.state().population;
+    if let Some(uuid) = req.query().get("node_uuid") {
+        let mut hasher = DefaultHasher::default();
+        uuid.hash(&mut hasher);
+        let client_uuid = hasher.finish();
+        if !population.maybe_contains(client_uuid) {
+            population.insert(client_uuid);
+            UNIQUE_IDS.inc();
+        }
+    }
 }
 
 #[derive(Debug, StructOpt)]
